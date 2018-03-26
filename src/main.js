41d7713410b8dd6ebd25e88869384394e9067221
Lucas <lucascomputacao@gmail.com>
2018-03-26 17:14:43 -0300

    check coverage prepush hook

--- src/main.js
+++ src/main.js
@@ -1,14 +1,10 @@
 const fizzBuzz = (number) => {
-  if(number === 0) return number;
-  if(number % 3 === 0 && number % 5 === 0) return 'FizzBuzz';
-  if(number % 3 === 0) return 'Fizz';
-  if(number % 5 === 0) return 'Buzz';
+  if (number === 0) return number;
+  if (number % 3 === 0 && number % 5 === 0) return 'FizzBuzz';
+  if (number % 3 === 0) return 'Fizz';
+  if (number % 5 === 0) return 'Buzz';
   // otherwise
   return number;
-}
+};
 
-function foo(){
-  return 'bla';
-}
-
-export {fizzBuzz}
+export default fizzBuzz;
